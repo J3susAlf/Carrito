f787540fe1cb3de39e96426a29780e3ef9ea7aef
Alfredo Magaña <89317157+J3susAlf@users.noreply.github.com>
2024-02-27 09:14:59 -0600

    SUBIENDO PROYECTO A GITHUB (#2)
    Co-authored-by: Alfredo Magaña <en2ruedas11@gmail.com>

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'Models/Productos.php';
 include 'Controllers/Carrito.php';
 include 'Template/Navbar.php';
 ?>
-
+<!--Subiendo el proyecto a github-->
 <div class="container mt-5 p-3">
     <!--SUBIENDO CAMBIOS A LA RAMA DEVELOP-->
     <br>
